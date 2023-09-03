414536b8d396295261839dd9891d85cf43cca2e4
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2023-09-03 17:11:55 +0100

    apply headers

--- src/app/records.service.ts
+++ src/app/records.service.ts
@@ -17,8 +17,7 @@ export class RecordsService {
   httpOptions = {
     headers: new HttpHeaders()
       .set('content-type', 'application/json')
-      .set('Access-Control-Allow-Origin', '*')
-      .set('Access-Control-Allow-Headers', '\'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token\'')
+      .set('Authorization', '')
   };
 
   getRecords(): Observable<Record[]> {
